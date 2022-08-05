ac19ccc28cdd50f8e12bc215dd0e2172d0f188a2
eric-lab-star <cyon256@icloud.com>
2022-08-05 15:28:50 +0900

    traveling pages

--- robot.js
+++ robot.js
@@ -1,13 +1,13 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
-const url = process.env.BASEURL;
-const course = process.env.COURSE_URL;
+import "dotenv/config";
+import puppeteer from "puppeteer";
+const BASEURL = process.env.BASEURL;
+const video = process.env.COURSE_URL;
 const userID = process.env.ID;
 const password = process.env.PASSWORD;
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(BASEURL);
   const nav = await page.$(`nav.relative > div.hidden a `);
   await nav.click();
   await page.keyboard.type(userID, { delay: 100 });
@@ -16,8 +16,20 @@ const password = process.env.PASSWORD;
   await page.keyboard.press("Tab");
   await page.keyboard.press("Tab");
   await page.keyboard.press("Enter");
-  await page.waitForSelector(`form.p-4`, { hidden: true, timeout: 10000 });
-  await page.goto(course);
-  const link = await page.$(`a.mt-2[href^="https://player.vimeo.com"]`);
-  await link.click();
+  await page.waitForSelector(`form.p-4`, { hidden: true, timeout: 5000 });
+  await page.goto(video);
+  let counter = 0;
+  const videos = await page.$$(`a.mt-1[href^="/courses/tower-defense/videos"]`);
+  while (counter < videos.length) {
+    await videos[counter].click();
+    await page.waitForTimeout(3000);
+    const downloadLink = await page.$(
+      `a.mt-2[href^="https://player.vimeo.com"]`
+    );
+    await downloadLink.click();
+    await page.waitForTimeout(3000);
+    counter = counter + 1;
+    console.log(counter);
+    if (counter === videos.length) console.log("complete!");
+  }
 })();
